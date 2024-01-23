885f895eaac04a2c1e8fa9c8b3c428ac56eea9ab
MehdiSaadati <108065298+MehdiSaadati0531@users.noreply.github.com>
2024-01-23 03:38:00 +0330

    Add files via upload

---
+++ Climbing_Stairs/Climbing_Stairs/Climbing_Stairs/Implement.cs
@@ -0,0 +1,27 @@
+﻿namespace Climbing_Stairs
+{
+    public class Implement
+    {
+        public int ClimbStairs(int n)
+        {
+            int first = 1;
+            int second = 2;
+            int result = 0;
+
+            if (n == 1)
+                return first;
+            if (n == 2)
+                return second;
+
+            for (int i = 2; i < n; i++)
+            {
+                result = first + second;
+                first = second;
+                second = result;
+            }
+
+            return result;
+        }
+    }
+
+}
