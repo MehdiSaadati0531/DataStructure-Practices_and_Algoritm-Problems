35063a6d1b83cc7e9423b3a1e51acc273d0de2b6
MehdiSaadati <108065298+MehdiSaadati0531@users.noreply.github.com>
2024-01-23 03:35:42 +0330

    Add files via upload

---
+++ CircularLinkedList/CircularLinkedList/Implement.cs
@@ -0,0 +1,93 @@
+﻿namespace CircularLinkedList
+{
+    class Node
+    {
+        public int Value;
+        public Node Next;
+        public Node Prev;
+    }
+
+
+    public class Implement
+    {
+        Node head = null;
+        Node lastItem = null;
+
+        private bool IsNull()
+        {
+            if (head == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Add(int val)
+        {
+            if (IsNull())
+            {
+                Node firstAdd = new Node();
+                firstAdd.Value = val;
+                firstAdd.Prev = firstAdd;
+                firstAdd.Next = firstAdd;
+                head = firstAdd;
+                lastItem = head;
+            }
+            else
+            {
+                Node newnode = new Node();
+                newnode.Value = val;
+
+                lastItem.Next = newnode;
+                newnode.Prev = lastItem;
+                newnode.Next = head;
+                lastItem = newnode;
+                head.Prev = lastItem;
+            }
+        }
+
+        public void Delete()
+        {
+            if (IsNull())
+            {
+                Console.WriteLine("Is Empty!!!");
+            }
+            else
+            {
+                if (lastItem == head)
+                {
+                    head = null;
+                }
+                else
+                {
+                    var temp = head.Next;
+                    head = null;
+                    temp.Prev = lastItem;
+                    lastItem.Next = temp;
+                    head = temp;
+                }
+            }
+        }
+
+        public void Print()
+        {
+            if (IsNull())
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine($"val=> {head.Value}");
+                var temp = head.Next;
+                while (temp != head)
+                {
+                    Console.WriteLine($"val=> {temp.Value}");
+                    temp = temp.Next;
+                }
+            }
+        }
+    }
+}
---
+++ CircularLinkedList/CircularLinkedList/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CircularLinkedList;
+
+Console.WriteLine("Hello, World!");
+
+
+Implement a = new Implement();
+
+a.Add(1);
+a.Add(2);
+a.Add(3);
+
+a.Delete();
+a.Delete();
+a.Delete();
+a.Delete();
+a.Delete();
+a.Delete();
+
+a.Print();
